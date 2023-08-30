88b9c4699b2b1000d09fed8f59b1a7cf8631e11a
diagui <tounkarakan@gmail.com>
2023-08-30 22:24:57 +0300

    first commit

---
+++ navigations/Navigator.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Navigator = () => {
+  return (
+    <View>
+      <Text>Navigator</Text>
+    </View>
+  )
+}
+
+export default Navigator
+
+const styles = StyleSheet.create({})
